f68b2dbadc9f90dc7091c05ea3a633bccc8d4263
nichen2 <nickychen434@gmail.com>
2023-08-21 20:16:31 -0700

    added local storage functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 const todoInput = document.querySelector('input[name="todo-input"]');
 const list = document.querySelector('ul')
+getLocal();
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -12,14 +13,24 @@ form.addEventListener("submit", function(e) {
     todo.appendChild(remove);
     list.appendChild(todo);
     todoInput.value = '';
+    setLocal();
 });
 
 list.addEventListener('click',function(e) {
     console.log(e.target.tagName);
     if (e.target.tagName === 'BUTTON') {
         e.target.parentElement.remove();
+        setLocal();
     }
     if (e.target.tagName === 'LI') {
         e.target.parentElement.classList.toggle("line-through")
     }
 });
+
+function setLocal() {
+    localStorage.setItem("todolist",JSON.stringify(list.innerHTML));
+}
+
+function getLocal() {
+    list.innerHTML = JSON.parse(localStorage.getItem("todolist"));
+}
